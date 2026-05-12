5fee3368f19433698630044db9ef08d6682144f5
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-12 10:11:50 +0200

    Add headers, corsOptions and fetch functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,34 @@
+import { constructURL } from "./utils.js";
 import express from "express";
 import cors from "cors";
 
-const PORT = process.env.PORT || 3000;
-const FRED_API_KEY = process.env.FRED_API_KEY;
+const { PORT } = process.env;
+
+const corsOptions = {
+  origin: ["https://dashboard.dineroybanca.com", `http://localhost:${PORT}`],
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+const headers = {
+  method: "GET",
+  headers: { "Content-Type": "application/json" },
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
+app.get("/api/:series_id", async (req, res) => {
+  try {
+    const url = constructURL(req);
+    const response = await fetch(url, { headers });
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 app.listen(PORT, () => {
