9f9851e8214a509ae990e441f8065c167cdc91a6
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-13 17:07:15 +0200

    Create SearchBar component and logic

---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,44 @@
+import { useSearch } from "../hooks/useSearch";
+import { useState } from "react";
+
+export default function SearchBar() {
+  const [inputValue, setInputValue] = useState("");
+  const [searchId, setSearchId] = useState("");
+  const { series, loading, error } = useSearch(searchId, {});
+
+  const handleSearch = () => {
+    if (inputValue.trim()) setSearchId(inputValue);
+  };
+
+  return (
+    <section>
+      <h1>
+        Dinero y <span>Banca</span>
+      </h1>
+
+      <article>
+        <input
+          type="text"
+          placeholder="Search data..."
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => handleSearch()}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            handleSearch;
+          }}
+          aria-label="Search"
+        >
+          Buscar
+        </button>
+      </article>
+
+      {loading && searchId && <p>Cargando datos de {searchId}...</p>}
+      {error && <p>Error: {error}</p>}
+
+      {series && <pre>{JSON.stringify(series, null, 2)}</pre>}
+    </section>
+  );
+}
