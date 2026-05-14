33f08933e0c149a87f1f83d149b961a9d037ef15
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-14 10:36:59 +0200

    Add Tanstack Query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import SearchBar from "./components/SearchBar";
 import "./App.css";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <SearchBar />
+      <QueryClientProvider client={queryClient}>
+        <SearchBar />
+      </QueryClientProvider>
     </>
   );
 }
