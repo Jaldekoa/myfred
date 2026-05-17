b579f3d2305c71c2b4f07fdbb93ad839c781727a
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-17 22:38:59 +0200

    Use filterParams to filter query params

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -2,12 +2,13 @@ const { FRED_API_URL, FRED_API_KEY } = process.env;
 
 function constructURL(req) {
   const { series_id } = req.params;
+  const filteredQuery = filterParams(req);
 
   const params = new URLSearchParams({
     series_id: series_id,
     api_key: FRED_API_KEY,
     file_type: "json",
-    ...req.query,
+    ...filteredQuery,
   });
 
   return `${FRED_API_URL}?${params.toString()}`;
