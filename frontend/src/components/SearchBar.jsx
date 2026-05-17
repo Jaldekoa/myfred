d1088a45268892ea9d77b2782d072eab063187ea
Jon Aldekoa <jaldekoa@gmail.com>
2026-05-17 21:22:53 +0200

    Refactor SearchBar component

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import { useSearch } from "../hooks/useSearch";
-import { useState } from "react";
+import Title from "./Title";
+import { useEffect, useState, useRef } from "react";
 import { Toaster, toast } from "sonner";
 
 export default function SearchBar() {
@@ -7,41 +8,44 @@ export default function SearchBar() {
   const [searchId, setSearchId] = useState("");
   const { series, loading, error } = useSearch(searchId, {});
 
+  const toastRef = useRef(null);
+
   const handleSearch = () => {
     if (inputValue.trim()) setSearchId(inputValue);
   };
 
+  useEffect(() => {
+    if (loading) {
+      toastRef.current = toast.loading(`Cargando ${searchId.toUpperCase()}...`);
+    }
+
+    if (!loading) {
+      if (toastRef.current) {
+        toast.dismiss(toastRef.current);
+        toastRef.current = null;
+      }
+      if (error) {
+        toast.error(`Error: ${error}`);
+      }
+    }
+  }, [loading, error]);
+
   return (
-    <section>
-      <h1>
-        Mi <span>App</span>
-      </h1>
+    <form>
+      <input
+        type="text"
+        placeholder="Search data..."
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+      />
+      <Toaster />
+      <button type="button" onClick={handleSearch} aria-label="Search">
+        Buscar
+      </button>
 
-      <article>
-        <input
-          type="text"
-          placeholder="Search data..."
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyDown={(e) => handleSearch()}
-        />
-        <Toaster />
-        <button
-          type="button"
-          onClick={() => {
-            toast.error("Hello from Sonner");
-            handleSearch;
-          }}
-          aria-label="Search"
-        >
-          Buscar
-        </button>
-      </article>
-
-      {loading && searchId && <p>Cargando datos de {searchId}...</p>}
       {error && <p>Error: {error}</p>}
-
       {series && <pre>{JSON.stringify(series, null, 2)}</pre>}
-    </section>
+    </form>
   );
 }
